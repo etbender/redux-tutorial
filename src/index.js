310cd19ed01ba2e3c79ac54cc78ee1f1ff1e171b
Eric Bender <etbender@edu.uwaterloo.ca>
2018-11-29 20:03:23 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import index from "./js/index"
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,3 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
